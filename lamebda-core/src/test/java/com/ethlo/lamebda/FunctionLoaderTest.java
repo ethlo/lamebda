2658c3ec4ab2c58bfee624bda53002f9864f5513
Morten <git@ethlo.com>
2018-10-26 18:04:16 +0200

    #9

--- lamebda-core/src/test/java/com/ethlo/lamebda/FunctionLoaderTest.java
+++ lamebda-core/src/test/java/com/ethlo/lamebda/FunctionLoaderTest.java
@@ -1,5 +1,25 @@
 package com.ethlo.lamebda;
 
+/*-
+ * #%L
+ * lamebda-core
+ * %%
+ * Copyright (C) 2018 Morten Haraldsen (ethlo)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
@@ -34,10 +54,10 @@ public class FunctionLoaderTest
     }
 
     @Test
-    public void testLoadOnCreate() throws IOException, InterruptedException
+    public void testLoadOnCreate() throws Exception
     {
         move("Correct.groovy");
-        Thread.sleep(1_000);
+        ioWait();
         final Map<String, ServerFunction> functions = functionManager.getFunctions();
         assertThat(functions.keySet()).containsExactly("Correct");
     }
@@ -46,18 +66,24 @@ public class FunctionLoaderTest
     public void testLoadOnModification() throws Exception
     {
         move("Correct.groovy");
-        Thread.sleep(1_000);
+        ioWait();
         move("Correct.groovy");
-        Thread.sleep(1_000);
+        ioWait();
+        return;
+    }
+
+    private void ioWait() throws InterruptedException
+    {
+        Thread.sleep(2_000);
     }
 
     @Test
     public void testUnloadOnRemoval() throws Exception
     {
         move("Correct.groovy");
-        Thread.sleep(1_000);
+        ioWait();
         remove("Correct.groovy");
-        Thread.sleep(1_000);
+        ioWait();
         final Map<String, ServerFunction> functions = functionManager.getFunctions();
         assertThat(functions.keySet()).doesNotContain("Correct");
     }
@@ -66,9 +92,9 @@ public class FunctionLoaderTest
     public void testUnloadOnError() throws Exception
     {
         final Path target = move("Correct.groovy");
-        Thread.sleep(1_000);
+        ioWait();
         Files.copy(Paths.get("src/test/groovy/Incorrect.groovy"), target, StandardCopyOption.REPLACE_EXISTING);
-        Thread.sleep(1_000);
+        ioWait();
         final Map<String, ServerFunction> functions = functionManager.getFunctions();
         assertThat(functions.keySet()).doesNotContain("Correct");
     }
---
+++ lamebda-springmvc/src/main/java/com/ethlo/lamebda/spring/SpringMvcServerFunction.java
@@ -0,0 +1,82 @@
+package com.ethlo.lamebda.spring;
+
+/*-
+ * #%L
+ * lamebda-spring-web-starter
+ * %%
+ * Copyright (C) 2018 Morten Haraldsen (ethlo)
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.util.Arrays;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import com.ethlo.lamebda.FunctionResult;
+import com.ethlo.lamebda.HttpRequest;
+import com.ethlo.lamebda.HttpResponse;
+import com.ethlo.lamebda.ServerFunction;
+
+public class SpringMvcServerFunction extends RequestMappingHandlerMapping implements ServerFunction
+{
+    protected final Logger logger = LoggerFactory.getLogger(getClass().getCanonicalName());
+
+    @Autowired
+    private RequestMappingHandlerAdapter adapter;
+
+    public SpringMvcServerFunction()
+    {
+        Arrays.asList(this.getClass().getMethods())
+                .stream()
+                .forEach(m -> {
+                    RequestMappingInfo mapping = getMappingForMethod(m, this.getClass());
+                    if (mapping != null)
+                    {
+                        registerMapping(mapping, this, m);
+                    }
+                });
+    }
+
+    @Override
+    public FunctionResult handle(final HttpRequest httpRequest, final HttpResponse httpResponse)
+    {
+        try
+        {
+            final HttpServletRequest rawRequest = (HttpServletRequest) httpRequest.raw();
+            final HttpServletResponse rawResponse = (HttpServletResponse) httpResponse.raw();
+            final HandlerExecutionChain handler = getHandler(rawRequest);
+            if (handler == null)
+            {
+                return FunctionResult.SKIPPED;
+            }
+            adapter.handle(rawRequest, rawResponse, handler.getHandler());
+        }
+        catch (final Exception e)
+        {
+            logger.error("There was a problem processing lamebda request", e);
+            httpResponse.error(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Unhandled error occurred while processing the request");
+        }
+        return FunctionResult.PROCESSED;
+    }
+}
