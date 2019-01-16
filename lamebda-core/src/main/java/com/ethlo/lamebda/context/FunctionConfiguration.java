a42a6f9638214519c671175acc7d45a5b85c46c4
Morten <git@ethlo.com>
2019-01-16 15:29:57 +0100

    Add better support for default values in config file

--- lamebda-core/src/main/java/com/ethlo/lamebda/context/FunctionConfiguration.java
+++ lamebda-core/src/main/java/com/ethlo/lamebda/context/FunctionConfiguration.java
@@ -9,9 +9,9 @@ package com.ethlo.lamebda.context;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,6 +22,7 @@ package com.ethlo.lamebda.context;
 
 import java.time.OffsetDateTime;
 import java.time.temporal.Temporal;
+import java.util.Optional;
 import java.util.Properties;
 import java.util.function.Function;
 
@@ -31,40 +32,60 @@ public class FunctionConfiguration extends Properties
 {
     public Integer getInt(String key)
     {
-        final String s = getString(key);
-        return assertNotNull(key, s, Integer.class, i->Integer.parseInt(s));
+        return assertNotNull(key, Integer::parseInt, null);
+    }
+
+    public Integer getInt(String key, Integer defaultValue)
+    {
+        return assertNotNull(key, Integer::parseInt, Optional.ofNullable(defaultValue));
     }
 
     public Long getLong(String key)
     {
-        final String s = getString(key);
-        return assertNotNull(key, s, Long.class, i->Long.parseLong(s));
+        return assertNotNull(key, Long::parseLong, null);
+    }
+
+    public Long getLong(String key, Long defaultValue)
+    {
+        return assertNotNull(key, Long::parseLong, Optional.ofNullable(defaultValue));
     }
 
     public OffsetDateTime getDateTime(String key)
     {
-        final String s = getString(key);
-        return assertNotNull(key, s, OffsetDateTime.class, i->ITU.parseDateTime(s));
+        return assertNotNull(key, ITU::parseDateTime, null);
+    }
+
+    public OffsetDateTime getDateTime(String key, OffsetDateTime defaultValue)
+    {
+        return assertNotNull(key, ITU::parseDateTime, Optional.ofNullable(defaultValue));
     }
 
     public Temporal getDate(String key)
     {
-        final String s = getString(key);
-        return assertNotNull(key, s, Temporal.class, i->ITU.parseLenient(s));
+        return assertNotNull(key, ITU::parseLenient, null);
+    }
+
+    public Temporal getDate(String key, Temporal defaultValue)
+    {
+        return assertNotNull(key, ITU::parseLenient, Optional.ofNullable(defaultValue));
     }
 
     public String getString(String key)
     {
-        final String s = getProperty(key);
-        return assertNotNull(key, s, String.class, i->s);
+        return assertNotNull(key, i -> i, Optional.empty());
     }
 
-    private <S, T> T assertNotNull(final String key, final S s, final Class<T> type, Function<S,T> converter)
+    private <T> T assertNotNull(final String key, Function<String, T> converter, Optional<T> defaultValue)
     {
-        if (s == null)
+        final String s = getProperty(key);
+        if (s != null)
+        {
+            return converter.apply(s);
+        }
+        else if (defaultValue == null)
         {
             throw new IllegalArgumentException("No value for configuration key " + key);
         }
-        return converter.apply(s);
+        return defaultValue.orElse(null);
     }
 }
--- lamebda-core/src/test/java/com/ethlo/lamebda/FunctionConfigurationTest.java
+++ lamebda-core/src/test/java/com/ethlo/lamebda/FunctionConfigurationTest.java
@@ -1,5 +1,44 @@
 package com.ethlo.lamebda;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import com.ethlo.lamebda.context.FunctionConfiguration;
+
 public class FunctionConfigurationTest
 {
+    private final FunctionConfiguration cfg = new FunctionConfiguration();
+
+    @Test
+    public void testExistingWithDefault()
+    {
+        cfg.setProperty("exists2", "2");
+        assertThat(cfg.getInt("exists2", 5)).isEqualTo(2);
+    }
+
+    @Test
+    public void testExistingWithoutDefault()
+    {
+        cfg.setProperty("exists1", "1");
+        assertThat(cfg.getInt("exists1")).isEqualTo(1);
+    }
+
+    @Test
+    public void testMissingWithDefault()
+    {
+        assertThat(cfg.getInt("foo", 123)).isEqualTo(123);
+    }
+
+    @Test
+    public void testMissingWithNullDefault()
+    {
+        assertThat(cfg.getInt("foo", null)).isNull();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMissingWithoutDefault()
+    {
+        cfg.getInt("foo");
+    }
 }
