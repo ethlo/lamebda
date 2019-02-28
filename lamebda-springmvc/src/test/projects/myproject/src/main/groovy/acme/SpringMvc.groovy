dde57f60f922d445bf2ab8118be8ef78e08572a6
mha <m.haraldsen@kezzler.com>
2019-02-28 23:47:29 +0100

    Fix tests

--- lamebda-springmvc/src/test/projects/myproject/src/main/groovy/acme/SpringMvc.groovy
+++ lamebda-springmvc/src/test/projects/myproject/src/main/groovy/acme/SpringMvc.groovy
@@ -1,5 +1,3 @@
-package mycontrollererer
-
 /*-
  * #%L
  * lamebda-springmvc
@@ -9,9 +7,9 @@ package mycontrollererer
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,9 +23,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
 class SpringMvc extends SpringMvcServerFunction
 {
     @PostMapping("test/{id}")
