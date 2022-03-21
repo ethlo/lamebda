11dc51fbca6dc91837fedaa7fe41796468c049de
Morten Haraldsen <m.haraldsen@kezzler.com>
2022-03-21 10:24:49 +0100

    Support for bypassing the root context. Generate proper configuration metadata

--- lamebda-springmvc/src/main/java/com/ethlo/lamebda/AllowAllLamebdaMetaAccessService.java
+++ lamebda-springmvc/src/main/java/com/ethlo/lamebda/AllowAllLamebdaMetaAccessService.java
@@ -9,9 +9,9 @@ package com.ethlo.lamebda;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,17 +24,10 @@ import javax.servlet.http.HttpServletRequest;
 
 public class LamebdaMetaAccessServiceImpl implements LamebdaMetaAccessService
 {
-    private final LamebdaConfiguration lamebdaConfiguration;
-
-    public LamebdaMetaAccessServiceImpl(LamebdaConfiguration lamebdaConfiguration)
-    {
-        this.lamebdaConfiguration = lamebdaConfiguration;
-    }
-
     @Override
     public boolean isIndexInfoAccessGranted(final HttpServletRequest request)
     {
-        return lamebdaConfiguration.isAllowIndexAccess();
+        return true;
     }
 
     @Override
