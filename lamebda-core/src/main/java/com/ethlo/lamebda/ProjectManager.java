2c2119cabb64cc1b9be7fbef44ab4704a11b68a2
Morten Haraldsen <dev@ethlo.com>
2025-11-07 08:08:44 +0100

    Fixes issue 65

--- lamebda-core/src/main/java/com/ethlo/lamebda/ProjectManager.java
+++ lamebda-core/src/main/java/com/ethlo/lamebda/ProjectManager.java
@@ -37,7 +37,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.Environment;
 import org.springframework.util.FileSystemUtils;
 
 import com.ethlo.lamebda.dao.LocalProjectDao;
@@ -56,7 +55,7 @@ public class ProjectManager
     private final LamebdaConfiguration rootConfiguration;
     private WatchDir watchDir;
 
-    public ProjectManager(final Environment environment, final LamebdaConfiguration lamebdaConfiguration, ConfigurableApplicationContext parentContext) throws IOException
+    public ProjectManager(final LamebdaConfiguration lamebdaConfiguration, ConfigurableApplicationContext parentContext) throws IOException
     {
         this.rootConfiguration = lamebdaConfiguration;
 
